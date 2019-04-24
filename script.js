4f74cb989b98350894900813b958c649b0c91b77
thuannguyen89 <235thuan@gmail.com>
2019-04-24 14:59:52 +0700

    nop bai tap lan 1

---
+++ script.js
@@ -0,0 +1,6 @@
+let inputWidth;
+let inputHeight;
+
+inputHeight = prompt("Enter the height")
+inputWidth = prompt("Enter the Width")
+
---
+++ script2.js
@@ -0,0 +1,4 @@
+let width = parseInt(inputWidth)
+let height = parseInt(inputHeight)
+let area = width * height;
+document.write("the area is: " + area);
